740f32a25f408ce34ff1b632cc3be7623479aae6
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2026-02-12 22:55:51 +0530

    Create language_list.dart

---
+++ mobile_app/lib/core/utils/language_list.dart
@@ -0,0 +1,10 @@
+class LanguageList {
+  static const Map<String,String> languages = {
+    "English":"en",
+    "Hindi":"hi",
+    "Spanish":"es",
+    "French":"fr",
+    "German":"de",
+    "Japanese":"ja",
+  };
+}
