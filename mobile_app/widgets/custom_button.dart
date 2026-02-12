c89c19fe77e50b2b9f85c84abebe9bff8fbd3dc3
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2026-02-12 22:39:37 +0530

    Create custom_button.dart

---
+++ mobile_app/widgets/custom_button.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onTap;
+
+  const CustomButton({super.key, required this.text, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 50,
+      child: ElevatedButton(
+        onPressed: onTap,
+        child: Text(text, style: TextStyle(fontSize: 18)),
+      ),
+    );
+  }
+}
