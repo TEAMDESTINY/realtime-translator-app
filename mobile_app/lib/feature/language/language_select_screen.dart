2a40381d3f0e89806057541be666a3ef58dc8d79
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2026-02-12 23:02:43 +0530

    Create language_select_screen.dart

---
+++ mobile_app/lib/feature/language/language_select_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import '../translator/translator_screen.dart';
+
+class LanguageSelectScreen extends StatefulWidget {
+  const LanguageSelectScreen({super.key});
+
+  @override
+  State<LanguageSelectScreen> createState() => _LanguageSelectScreenState();
+}
+
+class _LanguageSelectScreenState extends State<LanguageSelectScreen> {
+  String fromLang = "hi";
+  String toLang = "en";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Select Languages")),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          DropdownButton(value: fromLang, items: const [
+            DropdownMenuItem(value: "hi", child: Text("Hindi")),
+            DropdownMenuItem(value: "en", child: Text("English")),
+          ], onChanged: (v)=> setState(()=> fromLang=v!)),
+
+          DropdownButton(value: toLang, items: const [
+            DropdownMenuItem(value: "en", child: Text("English")),
+            DropdownMenuItem(value: "hi", child: Text("Hindi")),
+          ], onChanged: (v)=> setState(()=> toLang=v!)),
+
+          ElevatedButton(
+            child: const Text("Start"),
+            onPressed: (){
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (_) => TranslatorScreen(fromLang,toLang)));
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
